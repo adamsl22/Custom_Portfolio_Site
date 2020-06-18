d6b44b93cc397f897436f94274bbcfa76ce608a5
github username <aslongman22@gmail.com>
2020-06-18 19:26:39 -0400

    just gonna ppt the background

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -4,7 +4,7 @@ import Adam_pic from '../../assets/images/Adam_pic.JPG'
 export default function Header(){
     return(
         <div id="header">
-            <div align="left" id="header-img"><img src={Adam_pic} width={150} /></div>
+            <div align="left" id="header-img"><img src={Adam_pic} width={100} /></div>
             <div id="header-text">
                 <h1>Welcome!</h1>
                 <h2>I am a software engineer building websites and web apps.</h2>
